477d4381f5dfffeb933d1dc6fc2bb469253302b3
william <wcaicedo@unitecnologica.edu.co>
2016-03-02 16:27:03 -0500

    Initial clinical feature data

---
+++ public_html/js/data/catalog.js
@@ -0,0 +1,185 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+clinicalFeatures = [
+    {   id: 1,
+        name: "Hipotonicidad"
+    },
+    {   id: 2,
+        name: "Hipertonicidad"
+    },
+    {   id: 3,
+        name: "Ataxia"
+    },
+    {   id: 4,
+        name: "Sordera"
+    },
+    {   id: 5,
+        name: "Encefalocele"
+    },
+    {   id: 6,
+        name: "Hidrocefalia"
+    },
+    {   id: 7,
+        name: "Microcefalia"
+    },
+    {   id: 8,
+        name: "Macrocr\u00e1nea"
+    },
+    {   id: 9,
+        name: "Craniosinostosis y/o braquicefalia"
+    },
+    {   id: 10,
+        name: "Cierre tard\u00edo de fontanelas"
+    },
+    {   id: 11,
+        name: "Abombamiento frontal, frente y/o arcos supraorbitarios"
+    },
+    {   id: 12,
+        name: "Anomal\u00edas en el patr\u00f3n de cuero cabelludo y vello facial (incluye cabello ensortijado, proyecci\u00f3n a mejillas, etc.)"
+    },
+    {   id: 13,
+        name: "Facies plana"
+    },
+    {   id: 14,
+        name: "Facies redonda"
+    },
+    {   id: 15,
+        name: "Facies triangular"
+    },
+    {   id: 16,
+        name: "Facies en m\u00e1scara"
+    },
+    {   id: 17,
+        name: "Facies grosera"
+    },
+    {   id: 18,
+        name: "Asimetr\u00eda facial"
+    },
+    {   id: 19,
+        name: "Hipertelorismo o hipotelorismo"
+    },
+    {   id: 20,
+        name: "Distopia cantorum"
+    },
+    {   id: 21,
+        name: "Pliegue epicántico"
+    },
+    {   id: 22,
+        name: "Desviación de fisuras palpebrales"
+    },
+    {   id: 23,
+        name: "Sinofris"
+    },
+    {   id: 24,
+        name: "Proptosis y ojos prominentes"
+    },
+    {   id: 25,
+        name: "Ptosis, blefarofimosis o anquiloblefaron"
+    },
+    {   id: 26,
+        name: "Defectos en ductus lacrimales"
+    },
+    {   id: 27,
+        name: "Estrabismo"
+    },
+    {   id: 28,
+        name: "Nistagmus"
+    },
+    {   id: 29,
+        name: "Miopía"
+    },
+    {   id: 30,
+        name: "Escleras azules"
+    },
+    {   id: 31,
+        name: "Microftalmia"
+    },
+    {   id: 32,
+        name: "Coloboma o una de sus variantes"
+    },
+    {   id: 33,
+        name: "Anomalías del iris"
+    },
+    {   id: 34,
+        name: "Glaucoma"
+    },
+    {   id: 35,
+        name: "Anomalías del tamaño o la forma de la córnea"
+    },
+    {   id: 36,
+        name: "Opacidad corneana"
+    },
+    {   id: 37,
+        name: "Cataratas y/o luxación de cristalino"
+    },
+    {   id: 38,
+        name: "Anomalías de pigmentación retiniana"
+    },
+    {   id: 39,
+        name: "Desprendimiento de retina"
+    },
+    {   id: 40,
+        name: "Puente nasal deprimido"
+    },
+    {   id: 41,
+        name: "Puente nasal o nariz prominente"
+    },
+    {   id: 42,
+        name: "Nariz pequeña"
+    },
+    {   id: 43,
+        name: "Hipoplasia de alas nasales o nariz en \"pico de loro\""
+    },
+    {   id: 44,
+        name: "Atresia de coanas"
+    },
+    {   id: 45,
+        name: "Hipoplasia malar o mediofacial"
+    },
+    {   id: 46,
+        name: "Hipoplasia maxilar"
+    },
+    {   id: 47,
+        name: "Micrognatia e hipoplasia mandibular"
+    },
+    {   id: 48,
+        name: "Prognatismo"
+    },
+    {   id: 49,
+        name: "Labio leporino y/o paladar hendido y/o úvula bífida"
+    },
+    {   id: 50,
+        name: "Filtro anormal"
+    },
+    {   id: 51,
+        name: "Micro o macrostomía"
+    },
+    {   id: 52,
+        name: "Lengua hendida o irregular en forma y/o posición"
+    },
+    {   id: 53,
+        name: "Macroglosia o microglosia"
+    },
+    {   id: 54,
+        name: "Anomalías en la laringe"
+    },
+    {   id: 55,
+        name: "Anodoncia o hipodoncia"
+    },
+    {   id: 56,
+        name: "Otras anomalías dentales"
+    },
+    {   id: 57,
+        name: "Oidos de implantación baja o rotados"
+    },
+    {   id: 58,
+        name: "Pabellones auriculares grandes"
+    }
+    
+];
+
