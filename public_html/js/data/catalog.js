40a5adee0b051ba5436050ec13f8bac222315529
william <wcaicedo@unitecnologica.edu.co>
2016-03-03 11:16:36 -0500

    feature catalog complete

--- public_html/js/data/catalog.js
+++ public_html/js/data/catalog.js
@@ -6,180 +6,487 @@
 
 
 clinicalFeatures = [
-    {   id: 1,
+    {id: 1,
         name: "Hipotonicidad"
     },
-    {   id: 2,
+    {id: 2,
         name: "Hipertonicidad"
     },
-    {   id: 3,
+    {id: 3,
         name: "Ataxia"
     },
-    {   id: 4,
+    {id: 4,
         name: "Sordera"
     },
-    {   id: 5,
+    {id: 5,
         name: "Encefalocele"
     },
-    {   id: 6,
+    {id: 6,
         name: "Hidrocefalia"
     },
-    {   id: 7,
+    {id: 7,
         name: "Microcefalia"
     },
-    {   id: 8,
+    {id: 8,
         name: "Macrocr\u00e1nea"
     },
-    {   id: 9,
+    {id: 9,
         name: "Craniosinostosis y/o braquicefalia"
     },
-    {   id: 10,
+    {id: 10,
         name: "Cierre tard\u00edo de fontanelas"
     },
-    {   id: 11,
+    {id: 11,
         name: "Abombamiento frontal, frente y/o arcos supraorbitarios"
     },
-    {   id: 12,
+    {id: 12,
         name: "Anomal\u00edas en el patr\u00f3n de cuero cabelludo y vello facial (incluye cabello ensortijado, proyecci\u00f3n a mejillas, etc.)"
     },
-    {   id: 13,
+    {id: 13,
         name: "Facies plana"
     },
-    {   id: 14,
+    {id: 14,
         name: "Facies redonda"
     },
-    {   id: 15,
+    {id: 15,
         name: "Facies triangular"
     },
-    {   id: 16,
+    {id: 16,
         name: "Facies en m\u00e1scara"
     },
-    {   id: 17,
+    {id: 17,
         name: "Facies grosera"
     },
-    {   id: 18,
+    {id: 18,
         name: "Asimetr\u00eda facial"
     },
-    {   id: 19,
+    {id: 19,
         name: "Hipertelorismo o hipotelorismo"
     },
-    {   id: 20,
+    {id: 20,
         name: "Distopia cantorum"
     },
-    {   id: 21,
+    {id: 21,
         name: "Pliegue epicántico"
     },
-    {   id: 22,
+    {id: 22,
         name: "Desviación de fisuras palpebrales"
     },
-    {   id: 23,
+    {id: 23,
         name: "Sinofris"
     },
-    {   id: 24,
+    {id: 24,
         name: "Proptosis y ojos prominentes"
     },
-    {   id: 25,
+    {id: 25,
         name: "Ptosis, blefarofimosis o anquiloblefaron"
     },
-    {   id: 26,
+    {id: 26,
         name: "Defectos en ductus lacrimales"
     },
-    {   id: 27,
+    {id: 27,
         name: "Estrabismo"
     },
-    {   id: 28,
+    {id: 28,
         name: "Nistagmus"
     },
-    {   id: 29,
+    {id: 29,
         name: "Miopía"
     },
-    {   id: 30,
+    {id: 30,
         name: "Escleras azules"
     },
-    {   id: 31,
+    {id: 31,
         name: "Microftalmia"
     },
-    {   id: 32,
+    {id: 32,
         name: "Coloboma o una de sus variantes"
     },
-    {   id: 33,
+    {id: 33,
         name: "Anomalías del iris"
     },
-    {   id: 34,
+    {id: 34,
         name: "Glaucoma"
     },
-    {   id: 35,
+    {id: 35,
         name: "Anomalías del tamaño o la forma de la córnea"
     },
-    {   id: 36,
+    {id: 36,
         name: "Opacidad corneana"
     },
-    {   id: 37,
+    {id: 37,
         name: "Cataratas y/o luxación de cristalino"
     },
-    {   id: 38,
+    {id: 38,
         name: "Anomalías de pigmentación retiniana"
     },
-    {   id: 39,
+    {id: 39,
         name: "Desprendimiento de retina"
     },
-    {   id: 40,
+    {id: 40,
         name: "Puente nasal deprimido"
     },
-    {   id: 41,
+    {id: 41,
         name: "Puente nasal o nariz prominente"
     },
-    {   id: 42,
+    {id: 42,
         name: "Nariz pequeña"
     },
-    {   id: 43,
+    {id: 43,
         name: "Hipoplasia de alas nasales o nariz en \"pico de loro\""
     },
-    {   id: 44,
+    {id: 44,
         name: "Atresia de coanas"
     },
-    {   id: 45,
+    {id: 45,
         name: "Hipoplasia malar o mediofacial"
     },
-    {   id: 46,
+    {id: 46,
         name: "Hipoplasia maxilar"
     },
-    {   id: 47,
+    {id: 47,
         name: "Micrognatia e hipoplasia mandibular"
     },
-    {   id: 48,
+    {id: 48,
         name: "Prognatismo"
     },
-    {   id: 49,
+    {id: 49,
         name: "Labio leporino y/o paladar hendido y/o úvula bífida"
     },
-    {   id: 50,
+    {id: 50,
         name: "Filtro anormal"
     },
-    {   id: 51,
+    {id: 51,
         name: "Micro o macrostomía"
     },
-    {   id: 52,
+    {id: 52,
         name: "Lengua hendida o irregular en forma y/o posición"
     },
-    {   id: 53,
+    {id: 53,
         name: "Macroglosia o microglosia"
     },
-    {   id: 54,
+    {id: 54,
         name: "Anomalías en la laringe"
     },
-    {   id: 55,
+    {id: 55,
         name: "Anodoncia o hipodoncia"
     },
-    {   id: 56,
+    {id: 56,
         name: "Otras anomalías dentales"
     },
-    {   id: 57,
+    {id: 57,
         name: "Oidos de implantación baja o rotados"
     },
-    {   id: 58,
+    {id: 58,
         name: "Pabellones auriculares grandes"
+    },
+    {id: 59,
+        name: "Malformación auricular incluyendo apéndices pre-auriculares"
+    },
+    {id: 60,
+        name: "Cuello redundante"
+    },
+    {id: 61,
+        name: "Cuello corto"
+    },
+    {id: 62,
+        name: "Hipoplasia u otras anomalías de clavículas, costillas, omoplato"
+    },
+    {id: 63,
+        name: "Pectus excavatum o carinatum"
+    },
+    {id: 59,
+        name: "Malformación auricular incluyendo apéndices pre-auriculares"
+    },
+    {id: 60,
+        name: "Cuello redundante"
+    },
+    {id: 61,
+        name: "Cuello corto"
+    },
+    {id: 62,
+        name: "Hipoplasia u otras anomalías de clavículas costillas omoplato"
+    },
+    {id: 63,
+        name: "Pectus excavatum o carinatum"
+    },
+    {id: 64,
+        name: "Hipoplasia o ausencia del pectoral mayor"
+    },
+    {id: 65,
+        name: "Caja torácica pequeña o anormal"
+    },
+    {id: 66,
+        name: "Escoliosis y/o cífosis y/o lordosis"
+    },
+    {id: 67,
+        name: "Otros defectos vertebrales"
+    },
+    {id: 68,
+        name: "Aracnodactilia y/o manos y/o pies grandes"
+    },
+    {id: 69,
+        name: "Braquidactilia y/o manos y/o pies pequeños"
+    },
+    {id: 70,
+        name: "Camptodactilia"
+    },
+    {id: 71,
+        name: "Fracturas"
+    },
+    {id: 72,
+        name: "Extremidades cortas"
+    },
+    {id: 73,
+        name: "Reducción de extremidades"
+    },
+    {id: 74,
+        name: "Clinodactilia"
+    },
+    {id: 75,
+        name: "Dedos gruesos y/o falange distal anormal en forma o tamaño"
+    },
+    {id: 76,
+        name: "Anomalías del pulgar radio o grueso artejo hallux valgus"
+    },
+    {id: 77,
+        name: "Hipoplasia o agenesia metacarpíana o falángica"
+    },
+    {id: 78,
+        name: "Polidactilia"
+    },
+    {id: 79,
+        name: "Sindactilia"
+    },
+    {id: 80,
+        name: "Displasia patelar o de tibia encurvamiento de huesos largos"
+    },
+    {id: 81,
+        name: "Hipoplasia ungueal"
+    },
+    {id: 82,
+        name: "Transtornos en dermatoglifos o pliegues de ﬂexión hoyuelos"
+    },
+    {id: 83,
+        name: "Contracturas articulares o motilidad articular disminuida"
+    },
+    {id: 84,
+        name: "Hiperelasticidad o luxaciones articulares"
+    },
+    {id: 85,
+        name: "Pie equino varo"
+    },
+    {id: 86,
+        name: "Pigmentación anormal en piel"
+    },
+    {id: 87,
+        name: "Piel gruesa o ictiótica o redundante"
+    },
+    {id: 88,
+        name: "Hemangiomas o telangiectasias equímosis fístulas A-V várices"
+    },
+    {id: 89,
+        name: "Dermatitis fotosensitiva. eczema o eritema fragilidad capilar y/o de piel"
+    },
+    {id: 90,
+        name: "Alopecia"
+    },
+    {id: 91,
+        name: "Hirsutísmo"
+    },
+    {id: 92,
+        name: "Otras anomalías del cabello"
+    },
+    {id: 93,
+        name: "Anomalías de sudoración"
+    },
+    {id: 94,
+        name: "Cardiopatías"
+    },
+    {id: 95,
+        name: "Hernias onfalocele o defectos de la pared abdominal _"
+    },
+    {id: 96,
+        name: "Hepatomegalia hepatoesplenomegalía o malformaciones hepáticas"
+    },
+    {id: o,
+        name: "splénicas"
+    },
+    {id: 97,
+        name: "Fístula traqueoesofágica o atresia"
+    },
+    {id: 98,
+        name: "Malformación o disfunción renal o de vías urinarias"
+    },
+    {id: 99,
+        name: "Anomalía genital incluyendo criptorquidia"
+    },
+    {id: 100,
+        name: "Anomalías vaginales"
+    },
+    {id: 101,
+        name: "Defectos anorectales"
+    },
+    {id: 102,
+        name: "Hípogonadismo"
+    },
+    {id: 103,
+        name: "Otros transtornos endocrinológicos"
+    },
+    {id: 104,
+        name: "Lipoatrofia y/o mioatrofia"
+    },
+    {id: 105,
+        name: "Inmunodeficiencia"
+    },
+    {id: 106,
+        name: "Anemia"
+    },
+    {id: 107,
+        name: "Trombocitopenia pancitopenia"
+    },
+    {id: 108,
+        name: "Leucocitosis policitemia"
+    },
+    {id: 109,
+        name: "Malignidad linforeticular"
+    },
+    {id: 110,
+        name: "Otras malignidades"
+    },
+    {id: 111,
+        name: "Obesidad"
+    },
+    {id: 112,
+        name: "Macrosomía, sobrecrecimiento"
+    },
+    {id: 113,
+        name: "Asimetría corporal"
+    },
+    {id: 114,
+        name: "Retardo mental"
+    },
+    {id: 115,
+        name: "Baja talla retardo en el crecimiento"
+    },
+    {id: 116,
+        name: "Crecimiento acelerado o talla alta"
+    },
+    {id: 117,
+        name: "Atroﬁa o hipoplasia cerebelar"
+    },
+    {id: 118,
+        name: "Miotonia"
+    },
+    {id: 119,
+        name: "Quistes branquiales"
+    },
+    {id: 120,
+        name: "Tronco corto"
     }
-    
+
+];
+
+
+syndromes = [
+    {
+        id: 28,
+        name: "Síndrome de Carpenter",
+        synonym: " Acro-céfalo-poli-sindactilia tipo II",
+        inheritance: "Autosómica recesiva",
+        retardation: true,
+        evolution: "El tipo de anormalidades presentes en cada paciente\n\
+                determinarán sus limitaciones, pudiendo beneficiarse del \n\
+                tratamiento ortopédico oportuno. Es importante el diagnóstico temprano\n\
+                para corregir la craniosinostosis cuanto antes. Casi todos presentan defectos\n\
+                en la articulación del lenguaje y probemas de audición. No todos\n\
+                presentan retardo mental. La obesidad se inicia usualemente en \n\
+                la infancia.",
+        clinicalExams: "El diagnóstico se basa en el cuadro clínico y los hallazgos radiológicos.\n\
+                Se recomienda realizar pruebas auditivas para descartar sordera conductiva y/o neurosensorial.",
+        bibliography: ["GERSHONI-BARUCH, R. (1990). \"Carpenter Syndrome: Marked Variability of Expression\",\n\
+                American Journal of Medical Genetics, Vol. 35: 236-240."],
+        features: [9, 20, 21, 69, 70, 74, 78, 79, 82, 84, 94, 99, 111, 114]
+    },
+    {
+        id: 66,
+        name: "Síndrome de Saethre-Chotzen",
+        synonym: " Acro-céfalo-poli-sindactilia tipo III",
+        inheritance: "Autosómica dominante",
+        retardation: false,
+        retardationNotes:"(leve en pocos)",
+        evolution: "El cuadro clínico se presenta en la infancia y usualmente no afecta el promedio de vida.\n\
+                    La apariencia facial mejora con la edad. La inteligencia usualmente es normal aunque algunos\n\
+                    pacientes presentan retardo mental leve y aún no se ha aclarado si esto tiene relación con el\n\
+                    compromiso craneal. La lateralización de la entidad produce asimetría facial y plagiocefalia\n\
+                    estando el lado izquierdo comprometido con más frecuencia.",
+        clinicalExams: "El diagnóstico se basa en el cuadro clínico y radiológico. Se recomiendan RX de cráneo, cara\n\
+                    clavículas, manos y pies.",
+        bibliography: ["BIANCHI, E. et al. (1985). \" A Family with Saethre-Chotzen Syndrome\", American Journal of\n\
+                    Medical Genetics, Vol. 22: 649-658.", "FRIEDMAN, J. M. et al. (1977). \"Saethre-Chotzen Syndrome: A broad\n\
+                    and variable pattern of skeletal malformations\", Journal of Pediatrics, Vol. 91, No. 6, págs. 929-933"],
+        features: [9, 13, 18, 19, 25, 46, 59, 62, 69, 74, 75, 79, 82]
+    },
+    {
+        id: 24,
+        name: "Síndrome de Gorlin",
+        synonym: " Síndrome de carcinoma nevoide de células basales",
+        inheritance: "Autosómica dominante",
+        retardation: true,
+        retardationNotes:"",
+        evolution: "Las características aparecen en la infancia y se acentúan en la adolescencia.\n\
+                    Los nevus pueden malignizarse especialmente en la segunda década de la vida.\n\
+                    Las anomalías dentales pueden ser de forma y posición o presentarse como quistes y tumores\n\
+                    mandibulares y dentales. Pueden aparecer tumores en riñón, ovarios, meduloblastoma, etc.,\n\
+                    y de su detección temprana y adecuado manejo depende el pronóstico.",
+        clinicalExams: "El diagnóstico se basa en el cuadro clínico y radiológico. Es importante hacer un\n\
+                    seguimiento cercano a estos pacientes en busca de la aparición de quistes, tumores o\n\
+                    malignización de los nevus.",
+        bibliography: ["FERRIER, P. E. and HINRICHS, W. L. (1987). \"Basal-Cell Carcinoma Syndrome\", American\n\
+                    Journal of Diseases of Children, Vol. 113: 538-545.", "GORLIN, R. J. et al. (1990).\n\
+                    \"Syndromes of the Head and Neck\", Third Edition, Oxford University Press, págs. 372-380."],
+        features: [11, 41, 48, 56, 62, 86, 114]
+    },
+    {
+        id: 114,
+        name: "Osteodistrofia hereditaria de Albright",
+        synonym: "Pseudo-hipoparatiroidismo",
+        inheritance: "Dominante ligada a X",
+        retardation: true,
+        retardationNotes:"",
+        evolution: "Existe marcada variación fenotípica. La hipocalemia es aparente en la niñez\n\
+                    y las convulsiones son el síntoma más frecuente, manifestándose más en períodos de mayor\n\
+                    utilización de calcio como son la adolescencia y los embarazos. Puede requerirse manejo\n\
+                    con vitamina D. Se han descrito dos tipos en esta entidad, los cuales se pueden diferenciar\n\
+                    por su respuesta a la paratohormona: el tipo I se caracteriza por disminución en la excreción\n\
+                    urinaria de AMPc y en el tipo II su excreción es normal.",
+        clinicalExams: "El diagnóstico se basa en el cuadro clínico y oruebas bioquímicas que muestran las alteraciones\n\
+                        metabólicas (hipocalcemia o hiperfosfemia variables). Radiológicamente se observa\n\
+                        calcificaciones en tejidos blandos.",
+        bibliography: ["DIGEORGE, A. M. (1975). \"Albright syndrome: Is it coming of age?\", The Journal of\n\
+                        Pediatrics, Vol. 87, No. 6, págs. 1018-1020. ", "FITCH, N. (1982). \"Albright's Hereditary\n\
+                        Osteodystrophy: A Review\", American Journal of Medical Genetics, Vol. 11: 11-29."],
+        features: [14, 37, 40, 56, 61, 69, 77, 103, 111, 114, 115]
+    },
+    {
+        id: 17,
+        name: "Displasia Facio-Aurículo-Vertebral",
+        synonym: "Síndrome de Goldenhar",
+        inheritance: "Presentación esporádica",
+        retardation: false,
+        retardationNotes:"(presente en pocos)",
+        evolution: "El diagnóstico se hace desde el nacimiento y usualmente están comprometidas las estructuras\n\
+                    que se desarrollan a partir del 1o. y 2o. arcos branquiales. Al parecer la severidad del desorden\n\
+                    está dada por el nivel al cual desciende el techo de la cavidad timpánica y de si este está o no\n\
+                    por debajo del nivel de la fosa craneal media. Usualmente está indicada cirugía plástica.\n\
+                    Se ha visto retardo mental asociado cuando hay anomalías oculares serias como microftalmos.\n\
+                    Con frecuencia se encuentran en los ojos quistes dermoides epibulares uni o bilaterales.",
+        clinicalExams: "El diagnóstico es clínico y radiológico. Se sugiere realizar tomografía computarizada para\n\
+                    evaluar los huesos del oído medio como parte del estudio de audición.",
+        bibliography: ["FEINGOLD, M. and BAUM, J. \"Goldenhar's Syndrome\", Am j Dis Child, Vol. 132: 136-138. ", 
+                    "GORLIN, R. J. et al. (1990). \"Syndromes of the Head and Neck\", Third Edition, Oxford\n\
+                    University Press, págs. 641-646."],
+        features: [4, 18, 31, 32, 45, 46, 51, 59, 67]
+    }
 ];
 
