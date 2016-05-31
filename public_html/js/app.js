541e91138b05c06fd966ecc92ad670ae125f1c4f
william <wcaicedo@unitecnologica.edu.co>
2016-05-31 17:58:55 -0500

    list scroll position remembered across navigation

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -7,7 +7,7 @@
 
 
 app = angular.module('geneticSyndromeCatalogApp',['geneticSyndromeCatalogAppControllers', 'geneticSyndromeCatalogAppServices', 
-    'ngSanitize', 'ngRoute', 'angular-loading-bar', 'ui.select', 'ui.bootstrap', 'lodash']);
+    'geneticSyndromeCatalogAppDirectives', 'ngSanitize', 'ngRoute', 'angular-loading-bar', 'angular.filter', 'ui.select', 'ui.bootstrap', 'lodash']);
 
 app.filter('propsFilter', function() {
   return function(items, props) {
